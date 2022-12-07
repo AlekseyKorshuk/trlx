3fc56393393233f13c4f87e11bd3f597e3ca04b3
Aleksey Korshuk <ale-kor02@mail.ru>
2022-12-07 20:25:20 +0300

    feat: ilql_dalio

--- examples/dalio/ilql_dalio.py
+++ examples/dalio/ilql_dalio.py
@@ -129,6 +129,7 @@ class DalioModel(AccelerateILQLModel):
         stats["generate_time"] = time() - generate_time
 
         samples = self.accelerator.gather(torch.vstack(all_samples))
+        input_lengths = self.accelerator.gather(input_lengths)
         # input_texts = self.accelerator.gather(input_texts)
         # output_texts = self.accelerator.gather(output_texts)
 

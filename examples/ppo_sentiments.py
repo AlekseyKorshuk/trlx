6dee6e2ab87771266d243add200d5eacfbfcf3b5
Aleksey Korshuk <ale-kor02@mail.ru>
2022-12-13 00:23:37 +0300

    feat: gptj

--- examples/ppo_sentiments.py
+++ examples/ppo_sentiments.py
@@ -17,7 +17,7 @@ def get_positive_score(scores):
     return dict(map(lambda x: tuple(x.values()), scores))["POSITIVE"]
 
 
-default_config = yaml.safe_load(open("configs/ppo_config.yml"))
+default_config = yaml.safe_load(open("configs/ppo_gptj.yml"))
 
 
 def main(hparams={}):
